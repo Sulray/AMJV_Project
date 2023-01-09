871458b81ba9167197407c99ade53f616d7e5f07
Basile <bourgue.basile@gmail.com>
2023-01-09 13:15:44 +0100

    wave manager + pools

---
+++ Assets/Scripts/Entities/Ennemies/Pool.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pool : MonoBehaviour
+{
+    private Stack<Enemy> pool;
+    private Enemy prefab;
+    private int size;
+
+    public void createPool(int size, Enemy prefab)
+    {
+        pool = new Stack<Enemy>(size);
+        this.prefab = prefab;
+        this.size = size;
+    }
+
+    public Enemy GetEnemy()
+    {
+        if (pool.Count == 0)
+        {
+            Enemy enemy = Instantiate(prefab);
+            pool.Push(enemy);
+            return enemy;
+        }
+        else
+        {
+            Enemy enemy = pool.Pop();
+            enemy.enabled = true;
+            return enemy;
+        }
+    }
+
+    public void ReturnEnemy(Enemy enemy)
+    {
+        if(pool.Count >= size)
+        {
+            Destroy(enemy);
+        }
+        else
+        {
+            enemy.enabled = false;
+            pool.Push(enemy);
+        }
+    }
+}
---
+++ Assets/Scripts/Entities/Ennemies/WaveManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    [SerializeField] private Enemy[] enemies;
+    private Pool[] pools = new Pool[5];
+    //On utilise une Pool d'objets pour optimiser la mémoire
+    private void Awake()
+    {
+        int i = 0;
+        foreach(Enemy enemy in enemies)
+        {
+            pools[i] = new Pool();
+            i += 1;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
