ad20f70a80b34692bf9845fd79f0b781c2442c18
Ayris2 <celine78.kruse@gmail.com>
2023-01-18 11:23:14 +0100

    inputshero

--- Assets/Scripts/Entities/Characters/HeroController.cs
+++ Assets/Scripts/Entities/Characters/HeroController.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class HeroController : MonoBehaviour
+public class HeroController : Entity
 {
     [SerializeField] float directionOffset;
     NavMeshAgent agent;
     Animator animator;
-    [SerializeField] GameObject playerModel;    
+    [SerializeField] GameObject playerModel;
+    public bool inputAction1;
+    public bool inputAction2;
+    public bool inputACtion3;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,5 +25,12 @@ public class HeroController : MonoBehaviour
         agent.destination = transform.position + new Vector3(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical"))*directionOffset;
         animator.SetFloat("ForwardSpeed", agent.velocity.magnitude / agent.speed);
 
+    }
+
+    public void Inputs()
+    {
+        inputAction1 = Input.GetKeyDown(KeyCode.E);
+        inputAction2 = Input.GetKeyDown(KeyCode.R);
+        inputACtion3 = Input.GetKeyDown(KeyCode.C);
     }
 }
--- Assets/Scripts/Entities/Characters/Warrior.cs
+++ Assets/Scripts/Entities/Characters/Warrior.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 
 
 
-public class Warrior : Entity
+public class Warrior : HeroController
 {
-
 
 
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Inputs();
+        SwordHit();
     }
+
+    public void SwordHit()
+    {
+        if (inputAction1)
+        {
+            Debug.Log("Hit");
+        }
+    }
+
 }
