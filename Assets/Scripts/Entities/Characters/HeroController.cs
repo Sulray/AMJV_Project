4a034a2fbc488a473301de5f9caf52b171a86a8b
sulray <sulray250@gmail.com>
2023-01-18 12:24:54 +0100

    resolve conflict

--- Assets/Scripts/Entities/Characters/HeroController.cs
+++ Assets/Scripts/Entities/Characters/HeroController.cs
@@ -12,18 +12,28 @@ public class HeroController : Entity
     public bool inputAction1;
     public bool inputAction2;
     public bool inputACtion3;
+
+    float momentSpeed;
+    bool moving;
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         animator = playerModel.GetComponent<Animator>();
+        animator.SetFloat("Animation Speed", 1);
+        animator.SetTrigger("Trigger");
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        momentSpeed = agent.velocity.magnitude / agent.speed;
+        moving = (momentSpeed != 0);
         agent.destination = transform.position + new Vector3(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical"))*directionOffset;
-        animator.SetFloat("ForwardSpeed", agent.velocity.magnitude / agent.speed);
+        animator.SetFloat("Velocity",momentSpeed);
+        animator.SetBool("Moving", moving);
+
 
     }
 
