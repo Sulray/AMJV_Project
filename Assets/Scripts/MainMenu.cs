ad25fad145db613709edacab4879ad11f8ed007c
Lousch <schnee.louise@gmail.com>
2023-01-20 21:30:22 +0100

    settings menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class MainMenu : MonoBehaviour
 {
@@ -14,12 +17,38 @@ public class MainMenu : MonoBehaviour
     [SerializeField]
     GameObject settingsContainer;
 
+    [SerializeField]
+    TMP_Dropdown resolutionDropdown;
+
+    [SerializeField]
+    AudioMixer audioMixer;
+
+    Resolution[] resolutions;
+
     // Start is called before the first frame update
     void Start()
     {
         mainMenuContainer.SetActive(true);
         characterSelectionContainer.SetActive(false);
         settingsContainer.SetActive(false);
+
+        // Resolution settings set up
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
     }
 
     public void PlayGame()
@@ -49,5 +78,31 @@ public class MainMenu : MonoBehaviour
     public void StartGame()
     {
         // SceneManager.LoadScene(0);
+    }
+
+    // Setting functions
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+    public void SetMusicVolume(float musicVolume)
+    {
+        audioMixer.SetFloat("musicVolume", musicVolume);
+    }
+
+    public void SetEffectsVolume(float effectsVolume)
+    {
+        audioMixer.SetFloat("effectsVolume", effectsVolume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
