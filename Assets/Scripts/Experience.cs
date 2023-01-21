e75821da8bfd567ebe2f287fabeac4c3a0a32f0f
Lousch <schnee.louise@gmail.com>
2023-01-21 15:50:39 +0100

    created level system

---
+++ Assets/Scripts/Experience.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Experience : MonoBehaviour
+{
+    [SerializeField]
+    private int amount;
+    [SerializeField]
+    private float lifetime;
+
+    private LevelSystem levelSystem;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            levelSystem = other.GetComponent<LevelSystem>();
+            levelSystem.AddExeprience(amount);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/LevelSystem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSystem : MonoBehaviour
+{
+    private int level = 0;
+    private int experience = 0;
+
+    [SerializeField]
+    private int experienceToNextLevel;
+
+    public void AddExeprience(int amount)
+    {
+        experience += amount;
+        Debug.Log(experience);
+        if (experience >= experienceToNextLevel)
+        {
+            experience -= experienceToNextLevel;
+            NextLevel();
+        }
+    }
+
+    private void NextLevel()
+    {
+        level++;
+        Debug.Log("level up to" + level);
+        experienceToNextLevel = experienceToNextLevel*experienceToNextLevel;
+        Debug.Log("experience to next level: " + experienceToNextLevel);
+    }
+}
