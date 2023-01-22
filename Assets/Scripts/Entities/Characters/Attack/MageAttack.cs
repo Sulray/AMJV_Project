79275262c49831a3e916802b21d4d361d3ae06a1
sulray <sulray250@gmail.com>
2023-01-22 16:51:26 +0100

    merge

--- Assets/Scripts/Entities/Characters/Attack/MageAttack.cs
+++ Assets/Scripts/Entities/Characters/Attack/MageAttack.cs
@@ -1,13 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class MageAttack : Attack
 {
+    Object rotatingFireball;
+    Object baseFireball;
+
+
+
+    private void Start()
+    {
+        baseFireball = LoadPrefab("Assets/FireIceProjectileExplosion/Prefabs/FX_BulletTrail_Blue");
+        rotatingFireball = LoadPrefab("Assets/FireIceProjectileExplosion/Prefabs/Rotating_FX_BulletTrail_Blue");
+    }
+
+    private Object LoadPrefab(string filename)
+    {
+        var fireball = Resources.Load(filename);
+        if (fireball == null)
+        {
+            throw new FileNotFoundException("...no file found - please check the configuration");
+        }
+        return fireball;
+    }
 
     public override void First()
     {
         Debug.Log("Mage First Attack");
+        
     }
 
     public override void Second()
